3a9f77fd5bba90501f47c6519aceb12129d4d7a4
Erik Tegler <erik.tegler@gmail.com>
2018-11-29 11:50:16 +0100

    Update PredCorrProtocol.m

--- PredCorrProtocol.m
+++ PredCorrProtocol.m
@@ -1,4 +1,4 @@
-function [y_new] = PredCorrProtocol(t, y, f_old, f, h, tol, k)
+function [y_new, error] = PredCorrProtocol(t, y, f_old, f, h, tol, k)
 i = 0;
 i_max = 1*10^5;
 
@@ -6,9 +6,12 @@ order = num2str(k);
 ABk = eval(['@AB' order]);
 AMk = eval(['@AM' order]);
 
+
 y_pred = 10^5;
 y_corr = ABk(y, f_old, h); % Borde vara y_corr = inf; ?
-delta = tol;
+pred_save = y_corr;
+
+delta = 0.1;
 while abs(y_corr - y_pred) > tol
     if i < i_max
         y_pred = y_corr;
@@ -17,12 +20,13 @@ while abs(y_corr - y_pred) > tol
         g_y_old = AMk(y, f_old, f_pred, h);
         g_y_old_delta = AMk(y, f_old, f_delta, h);
         
-        y_corr = y_pred - delta*(y_pred - g_y_old)./(g_y_old_delta - g_y_old - delta);
+        y_corr = y_pred - delta*(y_pred - g_y_old)./(-g_y_old_delta + g_y_old + delta);
     else
         disp('No convergence')
         return
     end
     i = i+1;
 end
+error = abs(pred_save - y_corr);
 y_new = y_corr;
 end
